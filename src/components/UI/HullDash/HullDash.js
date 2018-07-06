f82c4ccfa99094986ff667a83130c9b161906ec1
Keenan Staffieri <kstaffieri@gmail.com>
2018-07-06 14:35:20 -0700

    hooking up play container to redux store

--- src/components/UI/HullDash/HullDash.js
+++ src/components/UI/HullDash/HullDash.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 import styles from './HullDash.css'
 
-const hullDash = () => (
+const hullDash = props => (
   <div className={styles.HullDash}>
     <h2>Hull Dash</h2>
     <span>Speed: 0</span>
     <span>Max Speed: 200</span>
     <span>Rotation: 0deg</span>
     <span>Shields: 100 / 100</span>
-    <span>Hull: 100 / 100</span>
+    <span>Hull: {props.hull} / {props.maxHull}</span>
     <span>Energy: 100 / 100</span>
     <span>Fuel: 100 / 100</span>
     <span>Mass: 30</span>
--- src/containers/Play/Play.js
+++ src/containers/Play/Play.js
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import GameCanvas from '../../components/GameCanvas/GameCanvas'
 import HullDash from '../../components/UI/HullDash/HullDash'
 
+import * as actions from '../../store/actions'
+
 import styles from './Play.css'
 
-const play = () => (
-  <div className={styles.PlayContainer}>
-    <GameCanvas />
-    <HullDash />
-  </div>
-)
+class Play extends Component {
+  render() {
+    return (
+      <div className={styles.PlayContainer}>
+        <GameCanvas />
+        <HullDash hull={this.props.hull} maxHull={this.props.maxHull} />
+      </div>
+    )
+  }
+}
 
-export default play
+const mapStateToProps = state => {
+  return {
+    hull: state.hull.hull,
+    maxHull: state.hull.maxHull
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onUpdateHull: hullPts => dispatch(actions.updateHull(hullPts))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Play)
