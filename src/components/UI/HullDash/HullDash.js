6edc5c855a23fac7b7f2778189788fabba8489ff
keenode <kstaffieri@gmail.com>
2018-07-06 20:10:14 -0700

    working on hull dash ui

--- src/components/UI/HullDash/HullDash.js
+++ src/components/UI/HullDash/HullDash.js
@@ -5,16 +5,8 @@ import styles from './HullDash.css'
 const hullDash = props => (
   <div className={styles.HullDash}>
     <div>
-      <h2>Hull Dash</h2>
-      <span>Speed: {props.speed}</span>
-      <span>Max Speed: {props.speedMax}</span>
-      <span>Rotation: {props.rotation}deg</span>
-      <span>Shields: {props.shields} / {props.shieldsMax}</span>
-      <span>Hull: {props.hull} / {props.hullMax}</span>
       <span>Energy: 100 / 100</span>
       <span>Fuel: 100 / 100</span>
-      <span>Mass: 30</span>
-      <span>Mode: Drive</span>
     </div>
     <div className={styles.HullVitals}>
       <div className={styles.VitalsRow}>
@@ -43,6 +35,14 @@ const hullDash = props => (
     <div className={styles.Speedometer}>
       <span className={styles.SpeedDisplay}>
         {Math.round(props.speed)}ps
+      </span>
+      <span className={styles.ModeDisplay}>
+        D
+      </span>
+    </div>
+    <div className={styles.RotationGuage}>
+      <span className={styles.RotationDisplay}>
+        {Math.round(props.rotation)}&deg;
       </span>
     </div>
   </div>
